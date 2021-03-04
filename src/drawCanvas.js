7eb80e3a98ba193533d9d7ef91f5e87bef1aea3c
Bigors Gábor <gabeszmaister@gmail.com>
2021-03-04 19:09:19 +0100

    Draw board
    I set the background and biselector line

--- src/drawCanvas.js
+++ src/drawCanvas.js
@@ -17,7 +17,22 @@ export default class gameBoard extends HTMLElement {
         canvas.setAttribute('width', this.width);
         canvas.setAttribute('height', this.height);
 
-
         this.shadowRoot.appendChild(canvas);
+        this.defineBoard();
+    }
+
+    defineBoard() {
+        // Define background
+        this.ctx.fillStyle = '#000000';
+        this.ctx.fillRect(0, 0, this.width, this.height);
+
+        // Define bisector
+        this.ctx.strokeStyle = '#ffffff';
+        this.ctx.beginPath();
+        this.ctx.setLineDash([5, 15]);
+        this.ctx.moveTo(this.width / 2, 0);
+        this.ctx.lineTo(this.width / 2, this.height);
+        this.ctx.stroke();
+        this.ctx.closePath();
     }
 }
